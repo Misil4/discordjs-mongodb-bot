9b5a9f1999bfc2536bcbae0f017044a0b8f7364a
misil470gamer <carlossilvamarques.cs@gmail.com>
2021-06-22 20:56:13 +0200

    0.5 Alpha 1

--- commands/newuser.js
+++ commands/newuser.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js");
 const index = require("./../index.js");
 module.exports.run = async (client, message, args) => {
+  let user = message.author;
 let Usuario =index.Usuario;
  if(!message.content.startsWith('!'))return;  
  function ShowUser() {
-  return Usuario.findOne({id: message.author.id});
+  return Usuario.findOne({id: user.id});
 }
 ShowUser().then(results => {
-    if (results.name == message.author.username){
-      message.reply("Usuario ya registrado")
-    } else {
+  if (!results) {
       const Nuevo = new Usuario({ id: message.author.id,name: message.author.username,money: 0,daily: 0,weekly:0,work: 0 });
       Nuevo.save(function (err, Nuevo) {
         if (err) return console.error(err);
         message.reply(":white_check_mark: Usuario Registrado correctamente");
       });
+  }
+  else if(results.name == message.author.username){
+    message.reply("Usuario ya registrado")
   }
 });
 }
